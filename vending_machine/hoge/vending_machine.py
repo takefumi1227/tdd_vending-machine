58f67e80f8794e0da40f9fb988eb0f45b804c1f0
takefumi1227 <spot.tb.soccer.take@gmail.com>
2020-10-01 12:34:47 +0900

    自販機が受け付けられる金額の変数名を変更

--- vending_machine/hoge/vending_machine.py
+++ vending_machine/hoge/vending_machine.py
@@ -1,8 +1,10 @@
 class VendingMachine:
+    ALLOWED_MONEY_LIST = [10, 50, 100, 500, 1000]
+
     def check_insert_money(self, insert_money):
-        if insert_money not in MONEY_LIST:
+        """
+        受け入れられる金額かどうかチェック
+        """
+        if insert_money not in self.ALLOWED_MONEY_LIST:
             return False
         return True
-
-
-MONEY_LIST = [10, 50, 100, 500, 1000]
